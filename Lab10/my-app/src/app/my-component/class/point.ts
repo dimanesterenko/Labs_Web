f0fab83d25f545166edb1facdcfc66da46964070
Dima Nesterenko <nesterenkoodmytro@gmail.com>
2023-11-25 01:27:37 +0200

    Add any for  Angular app

---
+++ Lab10/my-app/src/app/my-component/class/point.ts
@@ -0,0 +1,11 @@
+export class Point{
+    x:number;
+    y:number;
+    belong: boolean;
+    constructor(x:number, y:number,b:boolean){
+        this.x=x;
+        this.y=y;
+        this.belong=b;
+    }
+
+}
